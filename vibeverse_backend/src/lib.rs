c89d1ea7f260b633af6c340aea328b59531f6b2e
Sergej Sakac <sakacszergej@gmail.com>
2023-05-12 15:59:28 +0200

    Renaming on the backend

--- vibeverse_backend/src/lib.rs
+++ vibeverse_backend/src/lib.rs
@@ -2,10 +2,10 @@ use candid::Nat;
 use creators::Creator;
 use ic_cdk::export::Principal;
 use ic_cdk_macros::*;
-use portals::{Portal, PortalId};
+use nfts::{Nft, NftId};
 
 #[update]
-fn create_portal_blueprint(
+fn create_nft_blueprint(
     name: String,
     description: String,
     transferable: bool,
@@ -13,30 +13,30 @@ fn create_portal_blueprint(
     image_url: Option<String>,
 ) -> String {
     let creator = ic_cdk::api::caller();
-    portals::do_create_portal_blueprint(creator, name, description, transferable, limit, image_url);
+    nfts::do_create_nft_blueprint(creator, name, description, transferable, limit, image_url);
 
-    format!("Portal created successfully.")
+    format!("Nft created successfully.")
 }
 
 #[update]
-fn update_portal_metadata(
-    id: PortalId,
+fn update_nft_metadata(
+    id: NftId,
     name: String,
     description: String,
     image_url: Option<String>,
 ) -> String {
     let caller = ic_cdk::api::caller();
 
-    match portals::do_update_metadata(caller, id, name, description, image_url) {
+    match nfts::do_update_metadata(caller, id, name, description, image_url) {
         Ok(_) => format!("Metadata updated successfully."),
         Err(e) => format!("Error while updating metadata: {:?}", e),
     }
 }
 
-/// Get a specific portal with the provided `PortalId`.
+/// Get a specific nft with the provided `NftId`.
 #[ic_cdk_macros::query]
-fn get_portal(id: PortalId) -> Option<Portal> {
-    portals::do_get_portal(id)
+fn get_nft(id: NftId) -> Option<Nft> {
+    nfts::do_get_nft(id)
 }
 
 #[update]
@@ -54,40 +54,40 @@ fn get_creator_metadata() -> Option<Creator> {
 }
 
 #[ic_cdk_macros::query]
-fn get_portals_of_creator(creator: Principal) -> Vec<Portal> {
-    portals::do_get_portals_of_creator(creator)
+fn get_nfts_of_creator(creator: Principal) -> Vec<Nft> {
+    nfts::do_get_nfts_of_creator(creator)
 }
 
 #[ic_cdk_macros::query]
-fn get_portals_of_caller() -> Vec<Portal> {
+fn get_nfts_of_caller() -> Vec<Nft> {
     let caller = ic_cdk::api::caller();
-    portals::do_get_portals_of_creator(caller)
+    nfts::do_get_nfts_of_creator(caller)
 }
 
 #[update]
-fn mint_portal(portal: PortalId, receiver: Principal) -> String {
+fn mint_nft(nft: NftId, receiver: Principal) -> String {
     let caller = ic_cdk::api::caller();
-    match portals::do_mint_portal(caller, receiver, portal) {
-        Ok(_) => format!("Portal minted successfully."),
-        Err(e) => format!("Error while minting portal: {:?}", e),
+    match nfts::do_mint_nft(caller, receiver, nft) {
+        Ok(_) => format!("Nft minted successfully."),
+        Err(e) => format!("Error while minting nft: {:?}", e),
     }
 }
 
 #[update]
-fn transfer_portal(portal: PortalId, receiver: Principal) -> String {
+fn transfer_nft(nft: NftId, receiver: Principal) -> String {
     let caller = ic_cdk::api::caller();
-    match portals::do_transfer_portal(caller, receiver, portal) {
-        Ok(_) => format!("Portal transfered successfully."),
-        Err(e) => format!("Error while transfering the portal: {:?}", e),
+    match nfts::do_transfer_nft(caller, receiver, nft) {
+        Ok(_) => format!("Nft transfered successfully."),
+        Err(e) => format!("Error while transfering the nft: {:?}", e),
     }
 }
 
 #[ic_cdk_macros::query]
-fn get_portals_of_user(user: Principal) -> Vec<Portal> {
-    portals::do_get_portals_of_user(user)
+fn get_nfts_of_user(user: Principal) -> Vec<Nft> {
+    nfts::do_get_nfts_of_user(user)
 }
 
 #[ic_cdk_macros::query]
-fn get_portal_count() -> PortalId {
-    portals::do_get_portal_count()
+fn get_nft_count() -> NftId {
+    nfts::do_get_nft_count()
 }
