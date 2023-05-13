75068579a540517b6307f3d190189febe824f9c6
Roger Rangel <roger.rangel.ee@gmail.com>
2023-05-13 11:33:44 +0200

    Add Prettier

--- next.config.js
+++ next.config.js
@@ -1,20 +1,22 @@
-const DFXWebPackConfig = require("./webpack.config")
-DFXWebPackConfig.initCanisterIds()
+/* eslint-disable @typescript-eslint/no-var-requires */
+const DFXWebPackConfig = require('./webpack.config');
+DFXWebPackConfig.initCanisterIds();
 
-const webpack = require("webpack")
+const webpack = require('webpack');
 
 // Make DFX_NETWORK available to Web Browser with default "local" if DFX_NETWORK is undefined
 const EnvPlugin = new webpack.EnvironmentPlugin({
-  DFX_NETWORK: "local"
-})
+  DFX_NETWORK: 'local',
+});
 
 module.exports = {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Plugin
-    config.plugins.push(EnvPlugin)
+    config.plugins.push(EnvPlugin);
 
     // Important: return the modified config
-    return config
+    return config;
   },
-  output: "export"
-}
+  output: 'export',
+};
