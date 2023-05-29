0ea6e7403bcfd0e706392a7296c37907faa92e73
Roger Rangel <roger.rangel.ee@gmail.com>
2023-05-29 09:23:28 +0200

    Add link to dashboard on main route

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { Mixpanel } from '@/components/Mixpanel';
 import Item3D from '../components/3D/Asset3D';
 import { assets } from '../constants';
@@ -22,9 +23,12 @@ export default function Home() {
       />
 
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex py-10">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Demo Page | Buildspace S3
-        </p>
+        <Link href="/dashboard">
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Demo | Dashboard
+          </p>
+        </Link>
+       
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
