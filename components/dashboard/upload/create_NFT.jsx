c95b4faa83b9ab7779c49de27a007d8fa28f9656
Roger Rangel <roger.rangel.ee@gmail.com>
2023-06-08 21:47:29 +0200

    Mint UI input receiver

--- components/dashboard/upload/create_NFT.jsx
+++ components/dashboard/upload/create_NFT.jsx
@@ -273,6 +273,29 @@ function CreateNFT({ showCreateNFT }) {
                     </div>
                   </div>
                 </div>
+
+                <div className="col-span-full">
+                  <label
+                    htmlFor="username"
+                    className="block text-sm font-medium leading-6 text-white"
+                  >
+                    Receiver:
+                  </label>
+                  <div className="mt-2">
+                    <div className="flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+                      <input
+                        type="text"
+                        name="title"
+                        id="title"
+                        autoComplete="title"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        className="flex-1 border-0 bg-transparent py-1.5 pl-1 text-white focus:ring-0 sm:text-sm sm:leading-6"
+                        placeholder="wallet ID"
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
