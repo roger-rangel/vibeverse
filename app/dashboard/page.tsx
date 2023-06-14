8b675b4996b8c7b380cd9af741647d4f0e890aec
Roger Rangel <roger.rangel.ee@gmail.com>
2023-06-14 17:01:24 +0200

    Home page dashboard updated

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { useEffect } from 'react';
-import { Preview, Categories } from '@/components/dashboard/home';
+import { Preview } from '@/components/dashboard/home';
 import { Mixpanel } from '@/components/Mixpanel';
+import Sign from '@/components/dashboard/shorts/sign';
+import Header from '@/components/dashboard/aitools/header';
 
 export default function Dashboard() {
   useEffect(() => {
@@ -12,7 +14,8 @@ export default function Dashboard() {
   return (
     <div className="mx-auto px-4 py-10 sm:px-6 lg:px-8 bg-gradient-to-r from-[#8360c3] to-[#2ebf91] ">
       <Preview />
-      <Categories />
+      <Sign />
+      <Header />
     </div>
   );
 }
