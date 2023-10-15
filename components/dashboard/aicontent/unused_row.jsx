6ed8c7755e123b2f1d68e18992776c21901959bd
Roger Rangel <roger.rangel.ee@gmail.com>
2023-10-15 11:41:40 +0200

    Clean clode

---
+++ components/dashboard/aicontent/unused_row.jsx
@@ -0,0 +1,55 @@
+{/* second row */}
+{/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 sm:px-6 lg:px-8 gap-1 pt-1 px-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-1">
+            <div className="col-span-1">
+              <div className="bg-gradient-to-r from-gray-700 via-gray-900 to-black relative flex justify-between items-center rounded-lg h-full sm:h-48">
+                <Image
+                  className="max-w-[60%]"
+                  src={
+                    'https://cdn.pixelbin.io/v2/throbbing-poetry-5e04c5/original/NFT-ticket.png'
+                  }
+                  height={200}
+                  width={200}
+                  alt=""
+                />
+                <span className=" mx-auto xxs:h-16 xxs:w-16 sm:h-14 sm:w-14 rounded-full object-cover flex items-center justify-center text-white xs:text-3xl sm:text-base md:text-3xl  pr-6">
+                  Movie Tickets
+                </span>
+              </div>
+            </div>
+            <div className="col-span-1">
+              <div
+                className="bg-gradient-to-l from-pink-300 via-purple-300 to-indigo-400 relative flex flex-col rounded-lg h-full xxs:h-60 sm:h-48 items-center justify-center "
+                style={{
+                  backgroundImage:
+                    "url('https://cdn.pixelbin.io/v2/throbbing-poetry-5e04c5/original/min.png')",
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  backgroundRepeat: 'no-repeat',
+                }}
+              >
+                <h2 className="text-5xl text-white">새로운 대회</h2>
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-1 gap-1">
+            <div className="col-span-1">
+              <div
+                className="bg-gradient-to-r from-fuchsia-600 to-pink-600 relative flex flex-col items-center justify-center rounded-lg h-full xxs:h-48"
+                style={{
+                  backgroundImage:
+                    "url('https://cdn.pixelbin.io/v2/throbbing-poetry-5e04c5/original/trailer.png')",
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  backgroundRepeat: 'no-repeat',
+                }}
+              >
+                <h2
+                  className={`${wallpoet.className} text-white xs:text-2xl bg-black bg-opacity-40 w-full flex items-center justify-center py-8`}
+                >
+                  AI CHALLENGES
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div> */}
