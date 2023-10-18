4bfa9cf3d0c5e5f7f76da58bd75aaaed93ec09f8
neddy34 <13149596+neddy34@users.noreply.github.com>
2023-10-18 22:37:17 +0900

    refactor modal

--- src/components/Nft/NftCard.tsx
+++ src/components/Nft/NftCard.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import React, { useState } from 'react';
-import Image from 'next/image';
-import classnames from 'tailwindcss-classnames';
+import React from 'react';
+import { useModal } from 'react-modal-hook';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import classnames from 'tailwindcss-classnames';
 
 import { Nft } from '@/types';
 
@@ -11,40 +11,33 @@ import { TransferModal } from './TransferModal';
 import styles from './Items.module.scss';
 
 export function NftCard({ nft }: { nft: Nft }) {
-  const [modal, showModal] = useState(false);
+  const [showModal, hideModal] = useModal(
+    () => <TransferModal isOpen nft={nft} hideModal={hideModal} />,
+    [nft],
+  );
 
   return (
-    <>
-      <article
-        key={`${nft.collectionId}-${nft.id}`}
-        className={classnames(styles.item__item, 'flex flex-col items-center')}
-      >
-        <LazyLoadImage
-          src={nft.assetUrl || '/images/items/item_1.png'}
-          effect="blur"
-          width="350"
-          height="350"
-          alt={nft.name}
-          className="rounded-2xl object-cover w-[350px] h-[350px]"
-        />
-        <h3>{nft.name}</h3>
-        <div className={classnames(styles.item__item__cta)}>
-          <button
-            onClick={() => {
-              showModal(true);
-            }}
-            className="py-2 px-4 text-base rounded-lg bg-gradient-to-r from-[#a855f7] to-[#3b82f6] hover:from-[#4ade80] hover:to-[#3b82f6]"
-          >
-            Transfer
-          </button>
-        </div>
-      </article>
-
-      {modal && (
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <TransferModal nft={nft} showModal={showModal} />
-        </div>
-      )}
-    </>
+    <article
+      key={`${nft.collectionId}-${nft.id}`}
+      className={classnames(styles.item__item, 'flex flex-col items-center')}
+    >
+      <LazyLoadImage
+        src={nft.assetUrl || '/images/items/item_1.png'}
+        effect="blur"
+        width="350"
+        height="350"
+        alt={nft.name}
+        className="rounded-2xl object-cover w-[350px] h-[350px]"
+      />
+      <h3>{nft.name}</h3>
+      <div className={classnames(styles.item__item__cta)}>
+        <button
+          onClick={showModal}
+          className="py-2 px-4 text-base rounded-lg bg-gradient-to-r from-[#a855f7] to-[#3b82f6] hover:from-[#4ade80] hover:to-[#3b82f6]"
+        >
+          Transfer
+        </button>
+      </div>
+    </article>
   );
 }
--- src/components/Nft/TransferModal.tsx
+++ src/components/Nft/TransferModal.tsx
@@ -1,23 +1,21 @@
 'use client';
 
 import { useState } from 'react';
-import Image from 'next/image';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { Principal } from '@dfinity/principal';
 import { classnames } from 'tailwindcss-classnames';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 
+import { Modal, ModalProps } from '@/components/Modal';
 import { useActor } from '@/hooks';
 import { Nft } from '@/types';
 
 import styles from './Items.module.scss';
 
 export function TransferModal({
-  showModal,
   nft,
-}: {
-  showModal: React.Dispatch<React.SetStateAction<boolean>>;
-  nft: Nft;
-}) {
+  isOpen,
+  hideModal,
+}: { nft: Nft } & ModalProps) {
   const [receiver, setReceiver] = useState('');
   const { actor } = useActor();
   const transfer = async () => {
@@ -36,58 +34,56 @@ export function TransferModal({
   };
 
   return (
-    <>
-      <div className="">
-        <article
-          key="1"
-          className="bg-[#262626] p-8 rounded-3xl border border-indigo-600"
-        >
-          <h3 className="flex items-center justify-center text-zinc-300 pb-2">
-            {nft.name}
-          </h3>
-          <div className={classnames(styles.item__item__image)}>
-            <LazyLoadImage
-              src={nft.assetUrl || '/images/items/item_1.png'}
-              effect="blur"
-              width="350"
-              height="350"
-              alt={nft.name}
-              className="rounded-2xl object-cover w-[350px] h-[350px]"
+    <Modal isOpen={isOpen} hideModal={hideModal}>
+      <article
+        key="1"
+        className="bg-[#262626] p-8 rounded-3xl border border-indigo-600"
+      >
+        <h3 className="flex items-center justify-center text-zinc-300 pb-2">
+          {nft.name}
+        </h3>
+        <div className={classnames(styles.item__item__image)}>
+          <LazyLoadImage
+            src={nft.assetUrl || '/images/items/item_1.png'}
+            effect="blur"
+            width="350"
+            height="350"
+            alt={nft.name}
+            className="rounded-2xl object-cover w-[350px] h-[350px]"
+          />
+        </div>
+
+        <div className="sm:col-span-2 pt-4 pb-6">
+          <label className="block text-sm font-semibold leading-6 text-white">
+            Send it to:
+          </label>
+          <div className="mt-2.5">
+            <input
+              value={receiver}
+              onChange={(e) => setReceiver(e.target.value)}
+              type="text"
+              name="receiver"
+              id="receiver"
+              autoComplete="receiver"
+              className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
             />
           </div>
-
-          <div className="sm:col-span-2 pt-4 pb-6">
-            <label className="block text-sm font-semibold leading-6 text-white">
-              Send it to:
-            </label>
-            <div className="mt-2.5">
-              <input
-                value={receiver}
-                onChange={(e) => setReceiver(e.target.value)}
-                type="text"
-                name="receiver"
-                id="receiver"
-                autoComplete="receiver"
-                className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className={classnames(styles.item__item__cta)}>
-            <button
-              onClick={transfer}
-              className="py-2 px-4 text-white rounded-lg bg-gradient-to-r from-[#a855f7] to-[#3b82f6] hover:from-[#4ade80] hover:to-[#3b82f6]"
-            >
-              Transfer
-            </button>
-            <button
-              onClick={() => showModal(false)}
-              className="py-2 px-4 text-white rounded-lg bg-gradient-to-r from-[#a855f7] to-[#3b82f6] hover:from-[#4ade80] hover:to-[#3b82f6]"
-            >
-              Close
-            </button>
-          </div>
-        </article>
-      </div>
-    </>
+        </div>
+        <div className={classnames(styles.item__item__cta)}>
+          <button
+            onClick={transfer}
+            className="py-2 px-4 text-white rounded-lg bg-gradient-to-r from-[#a855f7] to-[#3b82f6] hover:from-[#4ade80] hover:to-[#3b82f6]"
+          >
+            Transfer
+          </button>
+          <button
+            onClick={hideModal}
+            className="py-2 px-4 text-white rounded-lg bg-gradient-to-r from-[#a855f7] to-[#3b82f6] hover:from-[#4ade80] hover:to-[#3b82f6]"
+          >
+            Close
+          </button>
+        </div>
+      </article>
+    </Modal>
   );
 }
