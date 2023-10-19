3513cf7104579ff23bcb43a8ad1df88fd6663a81
neddy34 <13149596+neddy34@users.noreply.github.com>
2023-10-19 16:52:30 +0900

    sync backend

--- src/declarations/vibeverse_backend/vibeverse_backend.did.d.ts
+++ src/declarations/vibeverse_backend/vibeverse_backend.did.d.ts
@@ -5,10 +5,12 @@ export interface Collection {
   'id' : bigint,
   'creator' : Principal,
   'image_url' : [] | [string],
+  'transferable' : boolean,
   'name' : string,
   'minted' : bigint,
   'description' : string,
   'limit' : [] | [bigint],
+  'category' : string,
 }
 export interface Creator { 'principal' : Principal, 'name' : string }
 export interface Nft {
@@ -17,42 +19,42 @@ export interface Nft {
   'description' : string,
   'asset_url' : [] | [string],
 }
+export type Result = { 'Ok' : [bigint, bigint] } |
+  { 'Err' : string };
+export type Result_1 = { 'Ok' : null } |
+  { 'Err' : string };
 export interface _SERVICE {
   'admin' : ActorMethod<[], [] | [Principal]>,
   'collection_count' : ActorMethod<[], bigint>,
   'collection_fee' : ActorMethod<[], bigint>,
+  'collections' : ActorMethod<
+    [[] | [bigint], [] | [bigint]],
+    Array<Collection>
+  >,
   'collections_created_by' : ActorMethod<[Principal], Array<Collection>>,
   'collections_created_by_caller' : ActorMethod<[], Array<Collection>>,
   'create_collection' : ActorMethod<
-    [string, string, boolean, [] | [bigint], [] | [string]],
-    string
+    [string, string, boolean, [] | [bigint], [] | [string], string],
+    bigint
   >,
   'creator_metadata' : ActorMethod<[], [] | [Creator]>,
   'get_collection' : ActorMethod<[bigint], [] | [Collection]>,
   'mint_fee' : ActorMethod<[], bigint>,
   'mint_nft' : ActorMethod<
     [bigint, Principal, string, string, [] | [string]],
-    string
+    Result
   >,
-  'nfts' : ActorMethod<[bigint, bigint, bigint], Array<Nft>>,
+  'nfts' : ActorMethod<[bigint, [] | [bigint], [] | [bigint]], Array<Nft>>,
   'nfts_of_caller' : ActorMethod<[], Array<Nft>>,
   'nfts_of_user' : ActorMethod<[Principal], Array<Nft>>,
-  'set_admin' : ActorMethod<[Principal], { 'Ok' : null } | { 'Err' : string }>,
-  'set_collection_fee' : ActorMethod<
-    [bigint],
-    { 'Ok' : null } |
-      { 'Err' : string }
-  >,
+  'set_admin' : ActorMethod<[Principal], Result_1>,
+  'set_collection_fee' : ActorMethod<[bigint], Result_1>,
   'set_creator_metadata' : ActorMethod<[string], string>,
-  'set_mint_fee' : ActorMethod<[bigint], { 'Ok' : null } | { 'Err' : string }>,
-  'set_vibe_token' : ActorMethod<
-    [Principal],
-    { 'Ok' : null } |
-      { 'Err' : string }
-  >,
+  'set_mint_fee' : ActorMethod<[bigint], Result_1>,
+  'set_vibe_token' : ActorMethod<[Principal], Result_1>,
   'transfer_nft' : ActorMethod<[bigint, bigint, Principal], string>,
   'update_collection_metadata' : ActorMethod<
-    [bigint, string, string, [] | [string]],
+    [bigint, string, string, [] | [string], [] | [string]],
     string
   >,
   'vibe_token' : ActorMethod<[], [] | [Principal]>,
--- src/declarations/vibeverse_backend/vibeverse_backend.did.js
+++ src/declarations/vibeverse_backend/vibeverse_backend.did.js
@@ -3,14 +3,20 @@ export const idlFactory = ({ IDL }) => {
     'id' : IDL.Nat,
     'creator' : IDL.Principal,
     'image_url' : IDL.Opt(IDL.Text),
+    'transferable' : IDL.Bool,
     'name' : IDL.Text,
     'minted' : IDL.Nat,
     'description' : IDL.Text,
     'limit' : IDL.Opt(IDL.Nat),
+    'category' : IDL.Text,
   });
   const Creator = IDL.Record({
     'principal' : IDL.Principal,
     'name' : IDL.Text,
+  });
+  const Result = IDL.Variant({
+    'Ok' : IDL.Tuple(IDL.Nat, IDL.Nat),
+    'Err' : IDL.Text,
   });
   const Nft = IDL.Record({
     'id' : IDL.Tuple(IDL.Nat, IDL.Nat),
@@ -18,64 +24,69 @@ export const idlFactory = ({ IDL }) => {
     'description' : IDL.Text,
     'asset_url' : IDL.Opt(IDL.Text),
   });
+  const Result_1 = IDL.Variant({ 'Ok' : IDL.Null, 'Err' : IDL.Text });
   return IDL.Service({
-    'admin' : IDL.Func([], [IDL.Opt(IDL.Principal)], []),
-    'collection_count' : IDL.Func([], [IDL.Nat], []),
-    'collection_fee' : IDL.Func([], [IDL.Nat64], []),
+    'admin' : IDL.Func([], [IDL.Opt(IDL.Principal)], ['query']),
+    'collection_count' : IDL.Func([], [IDL.Nat], ['query']),
+    'collection_fee' : IDL.Func([], [IDL.Nat64], ['query']),
+    'collections' : IDL.Func(
+        [IDL.Opt(IDL.Nat), IDL.Opt(IDL.Nat)],
+        [IDL.Vec(Collection)],
+        ['query'],
+      ),
     'collections_created_by' : IDL.Func(
         [IDL.Principal],
         [IDL.Vec(Collection)],
-        [],
+        ['query'],
       ),
-    'collections_created_by_caller' : IDL.Func([], [IDL.Vec(Collection)], []),
+    'collections_created_by_caller' : IDL.Func(
+        [],
+        [IDL.Vec(Collection)],
+        ['query'],
+      ),
     'create_collection' : IDL.Func(
-        [IDL.Text, IDL.Text, IDL.Bool, IDL.Opt(IDL.Nat), IDL.Opt(IDL.Text)],
-        [IDL.Text],
+        [
+          IDL.Text,
+          IDL.Text,
+          IDL.Bool,
+          IDL.Opt(IDL.Nat),
+          IDL.Opt(IDL.Text),
+          IDL.Text,
+        ],
+        [IDL.Nat],
         [],
       ),
-    'creator_metadata' : IDL.Func([], [IDL.Opt(Creator)], []),
-    'get_collection' : IDL.Func([IDL.Nat], [IDL.Opt(Collection)], []),
-    'mint_fee' : IDL.Func([], [IDL.Nat64], []),
+    'creator_metadata' : IDL.Func([], [IDL.Opt(Creator)], ['query']),
+    'get_collection' : IDL.Func([IDL.Nat], [IDL.Opt(Collection)], ['query']),
+    'mint_fee' : IDL.Func([], [IDL.Nat64], ['query']),
     'mint_nft' : IDL.Func(
         [IDL.Nat, IDL.Principal, IDL.Text, IDL.Text, IDL.Opt(IDL.Text)],
-        [IDL.Text],
+        [Result],
         [],
       ),
-    'nfts' : IDL.Func([IDL.Nat, IDL.Nat, IDL.Nat], [IDL.Vec(Nft)], []),
-    'nfts_of_caller' : IDL.Func([], [IDL.Vec(Nft)], []),
-    'nfts_of_user' : IDL.Func([IDL.Principal], [IDL.Vec(Nft)], []),
-    'set_admin' : IDL.Func(
-        [IDL.Principal],
-        [IDL.Variant({ 'Ok' : IDL.Null, 'Err' : IDL.Text })],
-        [],
+    'nfts' : IDL.Func(
+        [IDL.Nat, IDL.Opt(IDL.Nat), IDL.Opt(IDL.Nat)],
+        [IDL.Vec(Nft)],
+        ['query'],
       ),
-    'set_collection_fee' : IDL.Func(
-        [IDL.Nat64],
-        [IDL.Variant({ 'Ok' : IDL.Null, 'Err' : IDL.Text })],
-        [],
-      ),
+    'nfts_of_caller' : IDL.Func([], [IDL.Vec(Nft)], ['query']),
+    'nfts_of_user' : IDL.Func([IDL.Principal], [IDL.Vec(Nft)], ['query']),
+    'set_admin' : IDL.Func([IDL.Principal], [Result_1], []),
+    'set_collection_fee' : IDL.Func([IDL.Nat64], [Result_1], []),
     'set_creator_metadata' : IDL.Func([IDL.Text], [IDL.Text], []),
-    'set_mint_fee' : IDL.Func(
-        [IDL.Nat64],
-        [IDL.Variant({ 'Ok' : IDL.Null, 'Err' : IDL.Text })],
-        [],
-      ),
-    'set_vibe_token' : IDL.Func(
-        [IDL.Principal],
-        [IDL.Variant({ 'Ok' : IDL.Null, 'Err' : IDL.Text })],
-        [],
-      ),
+    'set_mint_fee' : IDL.Func([IDL.Nat64], [Result_1], []),
+    'set_vibe_token' : IDL.Func([IDL.Principal], [Result_1], []),
     'transfer_nft' : IDL.Func(
         [IDL.Nat, IDL.Nat, IDL.Principal],
         [IDL.Text],
         [],
       ),
     'update_collection_metadata' : IDL.Func(
-        [IDL.Nat, IDL.Text, IDL.Text, IDL.Opt(IDL.Text)],
+        [IDL.Nat, IDL.Text, IDL.Text, IDL.Opt(IDL.Text), IDL.Opt(IDL.Text)],
         [IDL.Text],
         [],
       ),
-    'vibe_token' : IDL.Func([], [IDL.Opt(IDL.Principal)], []),
+    'vibe_token' : IDL.Func([], [IDL.Opt(IDL.Principal)], ['query']),
   });
 };
 export const init = ({ IDL }) => { return []; };
