37eb7923f3c95d163804fd55c2ea342b5b8acc73
neddy34 <13149596+neddy34@users.noreply.github.com>
2023-11-06 01:53:49 -0500

    update script to support network

--- scripts/deploy-test-data.sh
+++ scripts/deploy-test-data.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 BACKEND=vibeverse_backend
 PRINCIPAL=$(dfx identity get-principal)
+NETWORK=${1:-local}
 
 # Register profile
-dfx canister call $BACKEND set_creator_metadata '("Test Creator", "https://avatars.githubusercontent.com/u/13149596?v=4")'
+dfx canister call $BACKEND set_creator_metadata '("Test Creator", "https://avatars.githubusercontent.com/u/13149596?v=4")' --network $NETWORK
 
 # Creat mock data
-dfx canister call $BACKEND create_collection '("Test collection", "Test collection description", true, null, null, "test")'
-dfx canister call $BACKEND mint_nft '(0, (principal "'$PRINCIPAL'"), "Test #1", "Test #1 description", (opt "https://cdn.pixelbin.io/v2/throbbing-poetry-5e04c5/original/trophy_3.png"))'
-dfx canister call $BACKEND mint_nft '(0, (principal "'$PRINCIPAL'"), "Test #2", "Test #2 description", (opt "https://cdn.pixelbin.io/v2/throbbing-poetry-5e04c5/original/trophy_5.png"))'
+dfx canister call $BACKEND create_collection '("Test collection", "Test collection description", true, null, null, "test")' --network $NETWORK
+dfx canister call $BACKEND mint_nft '(0, (principal "'$PRINCIPAL'"), "Test #1", "Test #1 description", (opt "https://cdn.pixelbin.io/v2/throbbing-poetry-5e04c5/original/trophy_3.png"))' --network $NETWORK
+dfx canister call $BACKEND mint_nft '(0, (principal "'$PRINCIPAL'"), "Test #2", "Test #2 description", (opt "https://cdn.pixelbin.io/v2/throbbing-poetry-5e04c5/original/trophy_5.png"))' --network $NETWORK
 
 # Add emojis
 dfx canister call $BACKEND add_emojis '(vec { "https://cdn.pixelbin.io/v2/throbbing-poetry-5e04c5/original/psychodelic_hands.gif";
@@ -19,4 +20,4 @@ dfx canister call $BACKEND add_emojis '(vec { "https://cdn.pixelbin.io/v2/throbb
                                               "https://cdn.pixelbin.io/v2/throbbing-poetry-5e04c5/original/watermelon.gif";
                                               "https://cdn.pixelbin.io/v2/throbbing-poetry-5e04c5/original/trophy_3.png";
                                               "https://cdn.pixelbin.io/v2/throbbing-poetry-5e04c5/original/trophy_5.png"
-                                              })'
+                                              })' --network $NETWORK
