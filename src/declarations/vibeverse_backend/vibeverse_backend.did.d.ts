05f8692c91eccdbcacc1ce7a08d57906cd91d18f
neddy34 <13149596+neddy34@users.noreply.github.com>
2023-11-16 09:14:32 -0500

    update generated file

--- src/declarations/vibeverse_backend/vibeverse_backend.did.d.ts
+++ src/declarations/vibeverse_backend/vibeverse_backend.did.d.ts
@@ -14,6 +14,16 @@ export interface Collection {
   'limit' : [] | [bigint],
   'category' : string,
 }
+export interface Community {
+  'c' : Principal,
+  'd' : string,
+  'f' : Array<Principal>,
+  'l' : string,
+  'm' : Array<Principal>,
+  'n' : string,
+  's' : string,
+  'v' : boolean,
+}
 export interface Creator { 'a' : string, 'n' : string }
 export interface Nft {
   'id' : [bigint, bigint],
@@ -28,9 +38,11 @@ export type Result_1 = { 'Ok' : bigint } |
   { 'Err' : string };
 export type Result_2 = { 'Ok' : AddRemoveReactionResult } |
   { 'Err' : string };
-export type Result_3 = { 'Ok' : [bigint, bigint] } |
+export type Result_3 = { 'Ok' : string } |
   { 'Err' : string };
-export type Result_4 = { 'Ok' : null } |
+export type Result_4 = { 'Ok' : [bigint, bigint] } |
+  { 'Err' : string };
+export type Result_5 = { 'Ok' : null } |
   { 'Err' : string };
 export interface _SERVICE {
   'add_admin' : ActorMethod<[Principal], Result>,
@@ -49,29 +61,34 @@ export interface _SERVICE {
     [string, string, boolean, [] | [bigint], [] | [string], string],
     Result_1
   >,
+  'create_community' : ActorMethod<[string, string, string, string], Result_3>,
   'creator_metadata' : ActorMethod<[Principal], [] | [Creator]>,
   'get_collection' : ActorMethod<[bigint], [] | [Collection]>,
+  'get_communities_created_by' : ActorMethod<[Principal], Array<Community>>,
+  'get_communities_joinned' : ActorMethod<[Principal], Array<Community>>,
   'get_emojis' : ActorMethod<[], Array<string>>,
   'get_nft_metadata' : ActorMethod<[bigint, bigint], [] | [NftMetadata]>,
   'is_admin' : ActorMethod<[Principal], boolean>,
+  'join_community' : ActorMethod<[string], Result>,
+  'leave_community' : ActorMethod<[string], Result>,
   'mint_fee' : ActorMethod<[], bigint>,
   'mint_nft' : ActorMethod<
     [bigint, Principal, string, string, [] | [string]],
-    Result_3
+    Result_4
   >,
   'nfts' : ActorMethod<[bigint, [] | [bigint], [] | [bigint]], Array<Nft>>,
   'nfts_of_caller' : ActorMethod<[], Array<Nft>>,
   'nfts_of_user' : ActorMethod<[Principal], Array<Nft>>,
   'remove_admin' : ActorMethod<[Principal], Result>,
   'remove_emojis' : ActorMethod<[Array<string>], Result_1>,
-  'set_collection_fee' : ActorMethod<[bigint], Result_4>,
-  'set_creator_metadata' : ActorMethod<[string, string], Result_4>,
-  'set_mint_fee' : ActorMethod<[bigint], Result_4>,
-  'set_vibe_token' : ActorMethod<[Principal], Result_4>,
-  'transfer_nft' : ActorMethod<[bigint, bigint, Principal], Result_4>,
+  'set_collection_fee' : ActorMethod<[bigint], Result_5>,
+  'set_creator_metadata' : ActorMethod<[string, string], Result_5>,
+  'set_mint_fee' : ActorMethod<[bigint], Result_5>,
+  'set_vibe_token' : ActorMethod<[Principal], Result_5>,
+  'transfer_nft' : ActorMethod<[bigint, bigint, Principal], Result_5>,
   'update_collection_metadata' : ActorMethod<
     [bigint, string, string, [] | [string], [] | [string]],
-    Result_4
+    Result_5
   >,
   'vibe_token' : ActorMethod<[], [] | [Principal]>,
 }
