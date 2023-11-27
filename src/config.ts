49e86c9d04975e44c75c213d8fb4e024ce584a37
neddy34 <13149596+neddy34@users.noreply.github.com>
2023-11-26 21:52:01 -0500

    refactor actor provider

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,16 @@
+import { canisterId, createActor } from '@/declarations/vibeverse_backend';
+
 type NETWORK_TYPE = 'local' | 'ic';
 
 export const DFX_NETWORK = (process.env.DFX_NETWORK || 'local') as NETWORK_TYPE;
 
 export const II_CANISTER_ID = process.env.CANISTER_ID_INTERNET_IDENTITY;
+
+export const host =
+  DFX_NETWORK === 'local' ? 'http://localhost:4943' : 'https://ic0.app';
+
+export const anonymousActor = createActor(canisterId, {
+  agentOptions: {
+    host,
+  },
+});
