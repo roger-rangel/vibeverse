ab802f3fb367b971ddc07abf72f7d20ae8480d34
Roger Rangel <roger.rangel.ee@gmail.com>
2023-12-29 13:53:59 +0100

    Start dashboard

---
+++ src/components/learning/Dashboard.jsx
@@ -0,0 +1,9 @@
+
+export function Dashboard() {
+
+  return (
+    <div className="mx-10 pt-8">
+      <h1 className="text-3xl font-bold">Learning</h1>
+    </div>
+  );
+}
--- src/components/learning/index.tsx
+++ src/components/learning/index.tsx
@@ -1,3 +1,4 @@
 export * from './LearningSection';
 export * from './CreateCourseModal';
 export * from './AllCourses';
+export * from './Dashboard';
