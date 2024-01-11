113b619a70b28be137db66e1ca5c0e3f968fe2be
neddy34 <13149596+neddy34@users.noreply.github.com>
2024-01-11 04:19:09 -0500

    add link

--- src/app/dashboard/browse/page.tsx
+++ src/app/dashboard/browse/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { useEffect } from 'react';
 import { Mixpanel } from '@/components/Mixpanel';
 
@@ -33,10 +34,10 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <div className="mx-auto px-4 sm:px-6 lg:px-8 flex flex-col gap-5 pt-8 pb-20  min-h-[inherit] bg-cover bg-[url('https://cdn.pixelbin.io/v2/throbbing-poetry-5e04c5/original/blue_sky.png')]">
-      <div className="relative w-full justify-center items-center mx-auto gap-1">
-        <div className="xs:flex xs:flex-wrap min-h-full w-full sm:grid sm:grid-cols-1 sm:grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 gap-1">
-          <div className="relative rounded-lg flex flex-col justify-between sm:h-full overflow-hidden">
+    <div className="mx-auto flex min-h-[inherit] flex-col gap-5 bg-[url('https://cdn.pixelbin.io/v2/throbbing-poetry-5e04c5/original/blue_sky.png')] bg-cover px-4 pb-20  pt-8 sm:px-6 lg:px-8">
+      <div className="relative mx-auto w-full items-center justify-center gap-1">
+        <div className="min-h-full w-full gap-1 xs:flex xs:flex-wrap sm:grid sm:grid-cols-1 sm:grid-rows-2 lg:grid-cols-2 lg:grid-rows-1">
+          <div className="relative flex flex-col justify-between overflow-hidden rounded-lg sm:h-full">
             <Image
               src="/images/dashboard/gate.png"
               alt=""
@@ -45,7 +46,7 @@ export default function Dashboard() {
               height={1000}
             />
             <div className="relative w-full">
-              <div className="relative flex justify-center items-center sm:w-full sm:-mb-20 sm:p-12 ">
+              <div className="relative flex items-center justify-center sm:-mb-20 sm:w-full sm:p-12 ">
                 <Image
                   src="/images/logos/white.png"
                   alt=""
@@ -61,49 +62,52 @@ export default function Dashboard() {
               </div>
               <div className="flex justify-between pt-6">
                 <div className="flex items-center">
-                  <button className="bg-lime-400 px-4 py-2 rounded-full text-lg ">
+                  <button className="rounded-full bg-lime-400 px-4 py-2 text-lg ">
                     <AiFillStar />
                   </button>
-                  <h2 className="ml-2 text-white text-base">
+                  <h2 className="ml-2 text-base text-white">
                     Featured Community
                   </h2>
                 </div>
-                <button className="bg-white hover:bg-black hover:text-white px-4 border py-2 rounded-2xl text-lg">
+                <Link
+                  href="/dashboard/communities/white-mirror"
+                  className="rounded-2xl border bg-white px-4 py-2 text-lg hover:cursor-pointer hover:bg-black hover:text-white"
+                >
                   Enter
-                </button>
+                </Link>
               </div>
             </div>
           </div>
 
-          <div className="h-full w-full grid grid-cols-1 sm:grid-cols-5 xxs:gap-y-1 sm:gap-1 gap-y-2 xxs:mt-1 xs:mt-0">
-            <div className=" col-span-1 sm:col-span-1 bg-gradient-to-b from-sky-950 to-violet-400 rounded-xl flex sm:flex-col items-center justify-between overflow-hidden h-full xxs:py-10 sm:py-0 sm:h-auto">
+          <div className="grid h-full w-full grid-cols-1 gap-y-2 xxs:mt-1 xxs:gap-y-1 xs:mt-0 sm:grid-cols-5 sm:gap-1">
+            <div className=" col-span-1 flex h-full items-center justify-between overflow-hidden rounded-xl bg-gradient-to-b from-sky-950 to-violet-400 xxs:py-10 sm:col-span-1 sm:h-auto sm:flex-col sm:py-0">
               <Image
                 src="/images/dashboard/hand.png"
                 alt="Image"
-                className="flex xxs:h-32 xs:h-36 object-contain xxs:-ml-10 xs:-ml-8 xxs:-mt-14 xs:-mt-10 sm:ml-0 "
+                className="flex object-contain xxs:-ml-10 xxs:-mt-14 xxs:h-32 xs:-ml-8 xs:-mt-10 xs:h-36 sm:ml-0 "
                 height={200}
                 width={200}
               />
               <div className="flex flex-col">
-                <span className="text-white transform sm:-rotate-90 whitespace-nowrap sm:-mb-4 xxs:text-lg xs:text-2xl">
+                <span className="transform whitespace-nowrap text-white xxs:text-lg xs:text-2xl sm:-mb-4 sm:-rotate-90">
                   Become an AI Artist
                 </span>
-                <button className="bg-white hover:bg-black hover:text-white px-4 border py-2 rounded-2xl text-lg mt-4 sm:invisible">
+                <button className="mt-4 rounded-2xl border bg-white px-4 py-2 text-lg hover:bg-black hover:text-white sm:invisible">
                   Take me there
                 </button>
               </div>
 
               <div className="flex flex-col">
-                <button className="bg-sky-950 flex items-center justify-center xxs:invisible sm:visible sm:h-8 sm:w-8 md:h-10 md:w-10 lg:h-12 lg:w-12 rounded-full text-lg xs:-mb-6 sm:-mb-6 lg:-mb-8 z-10 text-white">
+                <button className="z-10 flex items-center justify-center rounded-full bg-sky-950 text-lg text-white xxs:invisible xs:-mb-6 sm:visible sm:-mb-6 sm:h-8 sm:w-8 md:h-10 md:w-10 lg:-mb-8 lg:h-12 lg:w-12">
                   <BsArrowUp />
                 </button>
-                <button className="bg-violet-700 xs:invisible sm:visible sm:h-8 sm:w-8  md:h-10 md:w-10 lg:h-12 lg:w-12 rounded-full text-lg sm:mb-4 md:mb-6 lg:mb-8"></button>
+                <button className="rounded-full bg-violet-700 text-lg xs:invisible sm:visible  sm:mb-4 sm:h-8 sm:w-8 md:mb-6 md:h-10 md:w-10 lg:mb-8 lg:h-12 lg:w-12"></button>
               </div>
             </div>
 
-            <div className="col-span-3 sm:col-span-3 rounded-xl grid grid-cols-1 gap-y-1">
+            <div className="col-span-3 grid grid-cols-1 gap-y-1 rounded-xl sm:col-span-3">
               <div
-                className="bg-blue-200 rounded-xl h-48 sm:h-full"
+                className="h-48 rounded-xl bg-blue-200 sm:h-full"
                 style={{
                   backgroundImage: "url('/images/dashboard/ai_tree.png')",
                   backgroundSize: 'cover',
@@ -112,7 +116,7 @@ export default function Dashboard() {
                 }}
               ></div>
               <div
-                className="bg-blue-200 rounded-xl h-48 sm:h-full"
+                className="h-48 rounded-xl bg-blue-200 sm:h-full"
                 style={{
                   backgroundImage:
                     "url('/images/dashboard/curious_refuge.png')",
@@ -121,11 +125,11 @@ export default function Dashboard() {
                   backgroundRepeat: 'no-repeat',
                 }}
               >
-                <div className="flex flex-col items-center justify-between h-full">
-                  <button className="bg-red-400 hover:bg-yellow-500 hover:text-white px-8 border py-2 rounded-2xl text-lg mt-10">
+                <div className="flex h-full flex-col items-center justify-between">
+                  <button className="mt-10 rounded-2xl border bg-red-400 px-8 py-2 text-lg hover:bg-yellow-500 hover:text-white">
                     AI Film Course
                   </button>
-                  <div className="bg-black bg-opacity-40 flex h-12 w-full items-center justify-center">
+                  <div className="flex h-12 w-full items-center justify-center bg-black bg-opacity-40">
                     <Image
                       src="/images/logos/curious.png"
                       alt="Image"
@@ -138,12 +142,12 @@ export default function Dashboard() {
               </div>
             </div>
 
-            <div className="grid grid-cols-1 xs:grid-cols-1 gap-1">
-              <div className="bg-gradient-to-b from-green-300 via-yellow-300 to-pink-300 rounded-xl h-16 sm:h-full sm:pt-4 flex xs:flex-row xxs:items-center xxs:justify-center items-center sm:flex-col xxs:h-32 xxs:space-x-2 sm:space-x-0">
+            <div className="grid grid-cols-1 gap-1 xs:grid-cols-1">
+              <div className="flex h-16 items-center rounded-xl bg-gradient-to-b from-green-300 via-yellow-300 to-pink-300 xxs:h-32 xxs:items-center xxs:justify-center xxs:space-x-2 xs:flex-row sm:h-full sm:flex-col sm:space-x-0 sm:pt-4">
                 {creators.map((item) => (
                   <div key={item.id} className="">
                     <Image
-                      className="mx-auto xxs:h-12 xxs:w-12 xs:h-16 xs:w-16 sm:h-12 sm:w-12 rounded-full object-cover xxs:mb-0 sm:mb-2 border border-white"
+                      className="mx-auto rounded-full border border-white object-cover xxs:mb-0 xxs:h-12 xxs:w-12 xs:h-16 xs:w-16 sm:mb-2 sm:h-12 sm:w-12"
                       src={item.image}
                       height={200}
                       width={200}
@@ -151,20 +155,20 @@ export default function Dashboard() {
                     />
                   </div>
                 ))}
-                <h2 className="flex text-slate-700 items-center justify-center xs:text-5xl sm:text-base xs:pl-2 sm:pl-0 sm:py-4">
+                <h2 className="flex items-center justify-center text-slate-700 xs:pl-2 xs:text-5xl sm:py-4 sm:pl-0 sm:text-base">
                   Creators
                 </h2>
               </div>
 
-              <div className="grid grid-rows-3 xxs:hidden sm:grid gap-1">
-                <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-orange-400 to-rose-400 rounded-xl h-40 sm:h-full flex justify-center items-center">
-                  <h2 className=" text-white text-base py-4">Shorts</h2>
+              <div className="grid grid-rows-3 gap-1 xxs:hidden sm:grid">
+                <div className="flex h-40 items-center justify-center rounded-xl bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-orange-400 to-rose-400 sm:h-full">
+                  <h2 className=" py-4 text-base text-white">Shorts</h2>
                 </div>
-                <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-400 to-emerald-400 rounded-xl h-20 sm:h-full flex justify-center items-center">
-                  <h2 className="text-white text-base py-4">Top 10</h2>
+                <div className="flex h-20 items-center justify-center rounded-xl bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-400 to-emerald-400 sm:h-full">
+                  <h2 className="py-4 text-base text-white">Top 10</h2>
                 </div>
-                <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-green-300 to-purple-400 rounded-xl h-20 sm:h-full flex justify-center items-center">
-                  <h2 className="text-slate-700 text-base py-4">Courses</h2>
+                <div className="flex h-20 items-center justify-center rounded-xl bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-green-300 to-purple-400 sm:h-full">
+                  <h2 className="py-4 text-base text-slate-700">Courses</h2>
                 </div>
               </div>
             </div>
